2f52a488d2594b00edf9085b16b47dc9a9e1f6bd
bouchiba43 <bouchibaahmed43@gmail.com>
2024-12-15 22:36:47 +0100

    Backend: Setup project structure

---
+++ electronicscenter/src/main/java/com/ecommerce/electronicscenter/ElectronicscenterApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.electronicscenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicscenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicscenterApplication.class, args);
+	}
+
+}
---
+++ electronicscenter/src/test/java/com/ecommerce/electronicscenter/ElectronicscenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.electronicscenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicscenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
