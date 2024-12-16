588b83f2062c066ae9fadeda821cddb4f932b4ef
bouchiba43 <bouchibaahmed43@gmail.com>
2024-12-16 13:07:29 +0100

    Implemented Specification pattern

--- electronicscenter/src/main/java/com/ecommerce/electronicscenter/repositories/ProductRepository.java
+++ electronicscenter/src/main/java/com/ecommerce/electronicscenter/repositories/ProductRepository.java
@@ -1,9 +1,18 @@
 package com.ecommerce.electronicscenter.repositories;
 
 import com.ecommerce.electronicscenter.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    Page<Product> findAll(Specification<Product> spec, Pageable pageable);
+    Specification<Product> findByNameContaining(String Keyword);
+    Specification<Product> findByBrandId(Integer brandId);
+    Specification<Product> findByTypeId(Integer typeId);
+    Specification<Product> findByBrandIdAndTypeId(Integer brandId, Integer typeId);
 }
--- electronicscenter/src/main/java/com/ecommerce/electronicscenter/services/ProductServiceImpl.java
+++ electronicscenter/src/main/java/com/ecommerce/electronicscenter/services/ProductServiceImpl.java
@@ -4,6 +4,9 @@ import com.ecommerce.electronicscenter.entity.Product;
 import com.ecommerce.electronicscenter.models.ProductResponse;
 import com.ecommerce.electronicscenter.repositories.ProductRepository;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -41,13 +44,23 @@ public class ProductServiceImpl implements ProductService{
     }
 
     @Override
-    public List<ProductResponse> getProducts() {
+    public Page<ProductResponse> getProducts(Pageable pageable, Integer brandId, Integer typeId, String keyword) {
         log.info("Fetching Products...");
-        List<Product> productList = productRepository.findAll();
-        List<ProductResponse> productResponses = productList.stream()
-                .map(this::convertToProductResponse)
-                .collect(Collectors.toList());
+        Specification<Product> spec = Specification.where(null);
+        if(brandId != null){
+            spec = spec.and((root, query, criteriaBuilder) ->
+                    criteriaBuilder.equal(root.get("brand").get("id"), brandId));
+        }
+        if(typeId != null){
+            spec = spec.and((root, query, criteriaBuilder) ->
+                    criteriaBuilder.equal(root.get("type").get("id"), typeId));
+        }
+        if(keyword != null){
+            spec = spec.and((root, query, criteriaBuilder) ->
+                    criteriaBuilder.like(root.get("name"), "%"+keyword+"%"));
+        }
         log.info("Fetched All Products");
-        return productResponses;
+        return productRepository.findAll(spec, pageable)
+                .map(this::convertToProductResponse);
     }
 }
